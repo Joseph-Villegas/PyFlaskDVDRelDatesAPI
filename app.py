39ca7e548473b1c9b0df92816e97bfb77421ec30
Joseph-Villegas <33340560+Joseph-Villegas@users.noreply.github.com>
2021-03-14 22:21:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,44 @@
 """
 Filename: app.py
 Abstract: A flask api that performs a web 
-          scrape to retrieve current dvd release dates
+          scrape to retrieve current dvd release information
 """
 from flask import Flask, jsonify
-from dvd_bot import DVDBot
+from scraper import scrape, search_by_week
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'csumb-otter'
-
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>DVD Release Dates API</h1> \
         <p>This API retrieves retail release schedules for DVDs by performing a web scrape</p> \
         <p>Source: https://www.dvdsreleasedates.com</p> \
-        <p>Try these endpoints: /this-week, /last-week, /next-week, /in-two-weeks</p>"
-
+        <p>Try these endpoints: /this-week, /last-week, /next-week, /in-two-weeks, /all-weeks</p>"
 
 @app.route('/last-week', methods=['GET'])
-def lastWeek():
-    Bot = DVDBot()
-    week, dvds = Bot.getReleasesByWeek(1)
-    return jsonify({'weekOf': week, 'releases': dvds})
-
+def last_week():
+    scrape_results = search_by_week('last week')
+    return jsonify({'scrape_results': scrape_results, 'num_weeks': len(scrape_results)})
 
 @app.route('/this-week', methods=['GET'])
-def thisWeek():
-    Bot = DVDBot()
-    week, dvds = Bot.getReleasesByWeek(2)
-    return jsonify({'weekOf': week, 'releases': dvds})
-
+def this_week():
+    scrape_results = search_by_week('this week')
+    return jsonify({'scrape_results': scrape_results, 'num_weeks': len(scrape_results)})
 
 @app.route('/next-week', methods=['GET'])
-def nextWeek():
-    Bot = DVDBot()
-    week, dvds = Bot.getReleasesByWeek(3)
-    return jsonify({'weekOf': week, 'releases': dvds})
-
+def next_week():
+    scrape_results = search_by_week('next week')
+    return jsonify({'scrape_results': scrape_results, 'num_weeks': len(scrape_results)})
 
 @app.route('/in-two-weeks', methods=['GET'])
-def inTwoWeeks():
-    Bot = DVDBot()
-    week, dvds = Bot.getReleasesByWeek(0)
-    return jsonify({'weekOf': week, 'releases': dvds})
+def in_two_weeks():
+    scrape_results = search_by_week('in 2 weeks')
+    return jsonify({'scrape_results': scrape_results, 'num_weeks': len(scrape_results)})
 
+@app.route('/all-weeks', methods=['GET'])
+def all_weeks():
+    scrape_results = scrape()
+    return jsonify({'scrape_results': scrape_results, 'num_weeks': len(scrape_results)})
 
 if __name__ == "__main__":
     app.run()
